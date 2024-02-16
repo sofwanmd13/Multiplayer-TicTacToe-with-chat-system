c9077f47dc2861dfe129b8a6aea92c1a932c0e19
sofwanmd13 <sofwanmd701@yahoo.com>
2024-02-16 16:56:14 -0500

    completed the fully functional tic Tac toe game with sound effects and Play Again option

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,19 @@
+import GameState from "./GameState";
+
+function GameOver({gameState}) {
+    switch(gameState){
+        case GameState.inProgress:
+            return<></>;
+        case GameState.playerOWins:
+            return <div className="game-over">Player O Wins</div>;
+        case GameState.playerXWins:
+            return <div className="game-over">Player X Wins</div>;
+        case GameState.draw:
+            return <div className="game-over">Draw</div>;
+        default:
+            return <></>
+        
+    }
+}
+
+export default GameOver;
